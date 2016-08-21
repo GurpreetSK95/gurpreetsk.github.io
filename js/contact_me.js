c71341a34961e375cefa3119f3c36f5ed218b791
Gurpreet <gurpreetsk@hotmail.com>
2016-08-21 14:33:18 +0530

    - changes in about section, footer, header

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                email: "gurpreetsk@hotmail.com",
                 type: "POST",
                 data: {
                     name: name,
